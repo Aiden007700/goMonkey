d23584a21861e4a18d9e98961ae961323d0f1569
Aiden Campbell <acampbell@exadel.com>
2025-05-14 22:30:30 +0200

    repl

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/aiden007700/goMonkey/repl"
+	"os"
+	user2 "os/user"
+)
 
+func main() {
+	user, err := user2.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programing language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
